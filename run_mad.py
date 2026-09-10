35d460fd8477f950320baa767c3da0d4f33040c9
thaksheel <123469270+thaksheel@users.noreply.github.com>
2026-09-09 21:57:40 -0400

    sample 6000 stratif

--- run_mad.py
+++ run_mad.py
@@ -1,8 +1,9 @@
-import numpy as np 
-import pandas as pd 
-from matplotlib import pyplot as plt 
-import torch 
-import json 
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import torch
+import json
+from sklearn.model_selection import train_test_split
 
 from src.debate import Debate
 
@@ -11,9 +12,10 @@ with open("./exports/references.json", "r", encoding="utf-8") as f:
     references = json.load(f)
 filename = "./data/sr.csv"
 df = pd.read_csv(filename)
-df = df.dropna(subset=["review"]).sample(100)
-sentences = df.review.tolist()
-groundtruth = df.rating.to_numpy()
+df = df.dropna(subset=["review"])
+_, sample = train_test_split(df, train_size=0.8, random_state=42, stratify=df.rating)
+sentences = sample.review.tolist()
+groundtruth = sample.rating.to_numpy()
 debate = Debate(
     model_name=modelname,
     device="cuda",
@@ -23,7 +25,7 @@ debate = Debate(
     references=references,
 )
 final_cls, reasoning = debate.start_debate(sentences[0])
-results, reasonings = debate.simulate_debate(sentences) 
+results, reasonings = debate.simulate_debate(sentences)
 results = np.array(results)
 accurate = (groundtruth == results).sum()
 debate.save_reasoning()
