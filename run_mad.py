d7427b3784854421f21c2f1e8e4dcad7d30eb3b9
thaksheel <123469270+thaksheel@users.noreply.github.com>
2026-09-09 21:34:00 -0400

    added run file

--- run_mad.py
+++ run_mad.py
@@ -2,10 +2,26 @@ import numpy as np
 import pandas as pd 
 from matplotlib import pyplot as plt 
 import torch 
+import json 
 
+from src.debate import Debate
 
-# code 
 modelname = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-
-
+with open("./exports/references.json", "r", encoding="utf-8") as f:
+    references = json.load(f)
+filename = "./data/sr.csv"
+df = pd.read_csv(filename)
+sentences = df.dropna(subset=["review"]).sample(100).review.tolist()
+debate = Debate(
+    model_name=modelname,
+    device="cuda",
+    max_new_token=1000,
+    num_agents=5,
+    num_rounds=4,
+    references=references,
+)
+final_cls, reasoning = debate.start_debate(sentences[0])
+results, reasonings = debate.simulate_debate(sentences)
+debate.save_reasoning()
+print(final_cls, reasoning)
 print("END")
--- src/debate.py
+++ src/debate.py
@@ -89,30 +89,11 @@ class Debate:
         for text in sentences:
             r, re = self.start_debate(text)
             results.append(r)
-            reasoning.append(re)
+            reasonings.append(re)
         return results, reasonings
 
     def save_reasoning(self):
         pass
 
 
-if __name__ == "__main__":
-    modelname = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    with open("./exports/references.json", "r", encoding="utf-8") as f:
-        references = json.load(f)
-    filename = "./data/sr.csv"
-    df = pd.read_csv(filename)
-    sentences = df.dropna(subset=["review"]).sample(100).review.tolist()
-    debate = Debate(
-        model_name=modelname,
-        device="cuda",
-        max_new_token=1000,
-        num_agents=5,
-        num_rounds=4,
-        references=references,
-    )
-    final_cls, reasoning = debate.start_debate(sentences[0])
-    results, reasonings = debate.simulate_debate(sentences)
-    debate.save_reasoning()
-    print(final_cls, reasoning)
-    print("END")
+
